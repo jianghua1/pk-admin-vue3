9bfd4c13930597b4a36245929b6e71c55a91ea4d
jianghua1 <719584530@qq.com>
2024-09-29 15:50:35 +0800

    6.1.2.1 [回顾]使用自定义组件完成登录表单 第二部分

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    test
+  <div class="w-[900px] h-[600px] rounded">
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="密码登录" name="pass">
+        <VpForm :schema="loginFormSchema" @submit="handleSubmit"></VpForm>
+      </el-tab-pane>
+      <el-tab-pane label="验证码登录" name="code">Config</el-tab-pane>
+    </el-tabs>
   </div>
+
 </template>
 
 <script setup lang="tsx">
+import type { VpFormSchema } from "el-admin-components"
+import type { TabsPaneContext } from "element-plus/es/components/tabs/src/constants";
 definePage({
   meta: {
     title: '欢迎 回来',
@@ -14,25 +22,53 @@ definePage({
     backgroundImage: './bg.jpg'
   }
 })
+const activeName = ref('pass')
 
-const loginItems = [
+const userIcon = () => <i class="i-ep:user text-xl self-center"></i>
+
+const passIcon = () => <i class="i-ep:lock text-xl self-center"></i>
+
+const loginFormSchema = ref<VpFormSchema[]>([
   {
-    icon: 'ic:baseline-wechat',
-    url: ''
+    prop: 'username',
+    value: '',
+    attrs: {
+      placeholder: '请输入用户名'
+    },
+    type: 'input',
+    span: 24,
+    labelSlot: userIcon,
+    rules: [
+      {
+        required: true,
+        message: '用户名不能为空',
+        trigger: 'blur'
+      }
+    ]
   },
   {
-    icon: 'cib:tencent-qq',
-    url: ''
-  },
-  {
-    icon: 'bi:sina-weibo',
-    url: ''
-  },
-  {
-    icon: 'mdi:github',
-    url: ''
+    prop: 'password',
+    value: '',
+    attrs: {
+      placeholder: '请输入密码'
+    },
+    type: 'input',
+    span: 24,
+    labelSlot: passIcon,
+    rules: [
+      {
+        required: true,
+        message: '密码不能为空',
+        trigger: 'blur'
+      }
+    ]
   }
-]
+])
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
+
 
 const handleSubmit = (form: any) => {
   console.log('🚀 ~ file: login.vue:37 ~ handleSubmit ~ form:', form)

335502b75a0b8960a8fc58b26b1607774e7f32f4
jianghua1 <719584530@qq.com>
2024-10-06 20:05:07 +0800

    6.2.1.4 [扩展]消息机制：完成useWork方法封装

---
+++ src/hooks/useWork.ts
@@ -0,0 +1,62 @@
+export function useWork() {
+  const localId = ref('')
+  const routeIds = ref('')
+  const message = ref()
+
+  const events = new Map()
+  const worker = new SharedWorker(new URL('@/utils/shared-worker.ts', import.meta.url))
+
+  worker.port.start()
+
+  const route = useRoute()
+
+  worker.port.onmessage = function (event) {
+    const { type, id, eventName, data } = event.data
+    if (type === 'connected') {
+      localId.value = id
+      routeIds.value[id] = route.fullPath
+    } else {
+      //往指定的事件名发送消息
+      if (events.has(eventName)) {
+        const handlers = events.get(eventName)
+        handlers.forEach((handler) => handler(data))
+      }
+      //点对点消息
+      if (eventName === route.fullPath) {
+        const handlers = events.get('message') || []
+        handlers.forEach((handler) => handler(data))
+      }
+      console.log('收到消息：', event.data)
+      message.value = event.data
+    }
+  }
+  //发送消息
+  function emit(eventName: string, data: any) {
+    worker.port.postMessage({ type: 'emit', eventName, data })
+  }
+
+  function on(eventName: string, handler: Function) {
+    if (!events.has(eventName)) {
+      events.set(eventName, new Set())
+    }
+    events.get(eventName).add(handler)
+  }
+
+  function broadcast(data: any) {
+    worker.port.postMessage({ type: 'broadcast', data })
+  }
+
+  onUnmounted(() => {
+    // TODO 销毁event
+  })
+
+  return {
+    worker,
+    routeIds,
+    localId,
+    message,
+    emit,
+    on,
+    broadcast
+  }
+}

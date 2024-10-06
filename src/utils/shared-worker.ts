ccc3a3c777afceaf04b6a2595fa0c63388ead512
jianghua1 <719584530@qq.com>
2024-10-06 18:49:38 +0800

    6.2.1.3 [扩展]SharedWorker消息机制设计

--- src/utils/shared-worker.ts
+++ src/utils/shared-worker.ts
@@ -1,16 +1,45 @@
 /* eslint-disable no-undef */
-const ports = []
+const connections = {}
+let nextId = 0
+
+function generatedUnqueId() {
+  return `id-${nextId++}`
+}
+
+/**
+ * Broadcast a message to all ports in the connections object, except for the ports specified in the execludeIds array.
+ *
+ * @param {any} message - 收到的消息
+ * @param {string[]} execludeIds - 排除的id
+ */
+function broadcast(message, execludeId) {
+  Object.keys(connections).forEach(({ port, id }) => {
+    if (id !== execludeId) {
+      port.postMessage(message)
+    }
+  })
+}
+
 onconnect = function (e) {
   const port = e.ports[0] as MessagePort
-  ports.push(port)
+  const id = generatedUnqueId()
+  connections[id] = { port, id }
 
-  port.addEventListener('message', function (e) {
-    ports.forEach((p) => {
-      if (p !== port) {
-        p.postMessage(e.data)
-      }
-    })
+  port.addEventListener('message', function (event) {
+    const { type, data, eventName } = event.data
+    if (type === 'emit') {
+      broadcast({ eventName, data, formId: id }, id)
+    } else {
+      const sendData = type === 'broadcast' ? { eventName: 'message' } : {}
+      broadcast({ ...event.data, ...sendData, formId: id }, id)
+    }
   })
 
+  port.onclose = function () {
+    delete connections[id]
+    port.postMessage({ type: 'update', ids: Object.keys(connections) })
+  }
+
+  port.postMessage({ type: 'connected', id, ids: Object.keys(connections) })
   port.start() // Required when using addEventListener. Otherwise called implicitly by onmessage setter.
 }

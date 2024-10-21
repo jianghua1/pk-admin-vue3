c8a45e72596080c1e3873f9f16afb31bebb988dd
jianghua1 <719584530@qq.com>
2024-10-21 18:14:16 +0800

    6.3.4.4 优化拖拽上传逻辑：图片预览与删除

--- src/pages/contents/index.vue
+++ src/pages/contents/index.vue
@@ -151,11 +151,14 @@ const addEditFormSchema = ref<VpFormSchema>([
           <div>
             <div class="relative group w-full h-auto rounded border overflow-hidden">
               <img src={src} alt="" />
-              <div class="absolute w-full left-0 top-0 h-full flex text-white justify-around items-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+              <div class="absolute w-full left-0 top-0 h-full flex text-white 
+              justify-around items-center opacity-0 group-hover:opacity-100
+              transition-opacity duration-500 z-10">
                 <i class="i-ep:zoom-in text-3xl cursor-pointer" onClick={handlePreview}></i>
                 <i class="i-ep:delete text-3xl cursor-pointer" onClick={removePreview}></i>
               </div>
-              <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 transition-opacity duration-500 z-5"></div>
+              <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-40 
+              transition-opacity duration-500"></div>
             </div>
             <teleport to="body">
               <el-dialog v-model={show.value}>
